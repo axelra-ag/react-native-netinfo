acce06fe367ad0f7d92bc463c15920d9e32a9fda
Nicolas Gordillo <nicolas.gordillo@axelra.com>
2019-11-17 14:28:08 +0100

    ping cognito

--- src/internal/internetReachability.ts
+++ src/internal/internetReachability.ts
@@ -9,7 +9,7 @@
 
 import * as PrivateTypes from './privateTypes';
 
-const REACHABILITY_URL = 'https://clients3.google.com/generate_204';
+const REACHABILITY_URL = 'https://cognito-identity.eu-central-1.amazonaws.com/';
 const LONG_TIMEOUT = 60 * 1000; // 60s
 const SHORT_TIMEOUT = 5 * 1000; // 5s
 
